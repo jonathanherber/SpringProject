a9f0517132b03bc7f33713e59b18074571be3c7b
Jonathan <jonathanherber15@gmail.com>
2022-06-28 14:57:48 -0300

    services

---
+++ src/main/java/com/example/demo/services/UsersService.java
@@ -0,0 +1,26 @@
+package com.example.demo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entities.Users;
+import com.example.demo.repositories.UsersRepository;
+
+@Service
+public class UsersService {
+	
+	@Autowired
+	private UsersRepository repository;
+	
+	public List<Users> findAll(){
+		return repository.findAll();
+	}
+	
+	public Users findById(Long id) {
+		Optional<Users> obj = repository.findById(id);
+		return obj.get();
+	}
+}
