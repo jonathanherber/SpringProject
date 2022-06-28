0da18ddd34d307dcbf9c8421f39d2f6189ec40b0
Jonathan <jonathanherber15@gmail.com>
2022-06-28 14:28:02 -0300

    h2 database

--- src/main/java/com/example/demo/resources/UsersResource.java
+++ src/main/java/com/example/demo/resources/UsersResource.java
@@ -5,15 +5,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.entities.User;
+import com.example.demo.entities.Users;
 
 @RestController
 @RequestMapping(value="/users")
-public class UserResource {
+public class UsersResource {
 	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "jonathan", "jonathan@gmail.com", "999999", "12345");
+	public ResponseEntity<Users> findAll(){
+		Users u = new Users(1L, "jonathan", "jonathan@gmail.com", "999999", "12345");
 		return ResponseEntity.ok().body(u);
 	}
 	
