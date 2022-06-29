1ed1b5a66cd061933152b0b020be6ecbd68783af
Jonathan <jonathanherber15@gmail.com>
2022-06-29 16:25:44 -0300

    payment

--- src/main/java/com/example/demo/config/TestConfig.java
+++ src/main/java/com/example/demo/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.example.demo.entities.Category;
 import com.example.demo.entities.OrderItem;
 import com.example.demo.entities.Orders;
+import com.example.demo.entities.Payment;
 import com.example.demo.entities.Product;
 import com.example.demo.entities.Users;
 import com.example.demo.entities.enums.OrdersStatus;
@@ -82,6 +83,10 @@ public class TestConfig implements CommandLineRunner{
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		ordersRepository.save(o1);
 	}
 	
 	
--- src/main/java/com/example/demo/entities/Orders.java
+++ src/main/java/com/example/demo/entities/Orders.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,6 +13,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
 import com.example.demo.entities.enums.OrdersStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -35,6 +37,10 @@ public class Orders implements Serializable {
 
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
+
+	@OneToOne(mappedBy="order",cascade=CascadeType.ALL)
+	private Payment payment;
+	
 	
 	public Orders() {
 	}
@@ -95,6 +101,14 @@ public class Orders implements Serializable {
 		return sum;
 	}
 	
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
